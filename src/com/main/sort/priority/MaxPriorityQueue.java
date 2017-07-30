e916c019e3a2ae08ca5ea65023b002e3004de51e
Abel-Huang <497720975@qq.com>
2017-07-30 23:01:54 +0800

    heap MaxPriorityQueue

--- src/com/main/sort/priority/MaxPriorityQueue.java
+++ src/com/main/sort/priority/MaxPriorityQueue.java
@@ -1,17 +1,15 @@
-package com.main.sort;
+package com.main.sort.priority;
 
 /**
  * Created by huangjianjin on 2017/7/20.
  * 定义一个最大优先的优先队列
  */
-public class MaxPriorityQueue<K extends Comparable<K>> {
+public class MaxPriorityQueue<K extends Comparable<K>> extends PriorityQueue {
     public MaxPriorityQueue(){}
 
     public MaxPriorityQueue(int size){}
 
     public MaxPriorityQueue(K []k){}
-
-    public void insert(K k){}
 
     public K max(){
         return null;
@@ -19,13 +17,5 @@ public class MaxPriorityQueue<K extends Comparable<K>> {
 
     public K delMax(){
         return null;
-    }
-
-    public boolean isEmpty(){
-        return false;
-    }
-
-    public int size(){
-        return 0;
     }
 }
