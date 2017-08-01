9e71f7258d0b74d7ef4b6a805d2f191dbc246e5f
Abel-Huang <497720975@qq.com>
2017-08-02 00:41:58 +0800

    basic heap sort

---
+++ src/com/main/sort/priority/heap/HeapSort.java
@@ -0,0 +1,41 @@
+package com.main.sort.priority.heap;
+
+import com.main.basic.util.RandomData;
+import com.main.basic.util.StdIO;
+import com.main.sort.BasicSort;
+
+/**
+ * Created by huangjinajin on 2017/7/31.
+ */
+public class HeapSort extends BasicSort{
+    public void sort(Comparable [] a){
+        int N = a.length-1;
+        for (int k =N/2; k>=0; k--){
+            sink(a, k, N);
+        }
+        while (N > 0){
+            exchange(a, 0, N--);
+            sink(a, 0, N);
+        }
+    }
+
+    private void sink(Comparable [] a, int k, int N){
+        while(2*k+1 <= N){
+            int j = 2*k + 1;
+            if(j < N  && less(a[j], a[j+1])){
+                j++;
+            }
+            if (!less(a[k], a[j]))
+                break;
+            exchange(a, k, j);
+            k = j;
+        }
+    }
+
+    public static void main(String args[]){
+        Integer []a =RandomData.randIntegerArray(10, 10);
+        StdIO.print(a);
+        new HeapSort().sort(a);
+        StdIO.print(a);
+    }
+}
